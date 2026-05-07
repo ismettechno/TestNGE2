ca10940af8a4d23c37fa09eaea3af691eb6bb49e
TechnoStudy <ismettechno@gmail.com>
2026-05-07 19:18:07 +0100

    _05_Enable_2

---
+++ src/Gun01/_05_Enable_2.java
@@ -0,0 +1,25 @@
+package Gun01;
+
+import Utility.BaseDriver;
+import org.testng.annotations.Test;
+
+public class _05_Enable_2 extends BaseDriver {
+
+    @Test
+    public void Test1(){
+        System.out.println("test 1 çalıştı");
+        driver.get("https://www.facebook.com/");
+    }
+
+    @Test(enabled = false)
+    public void Test2(){
+        System.out.println("test 2 çalıştı");
+    }
+
+    @Test
+    public void Test3(){
+        System.out.println("test 3 çalıştı");
+        driver.get("https://www.google.com/");
+    }
+
+}
---
+++ src/Utility/BaseDriver.java
@@ -0,0 +1,33 @@
+package Utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public class BaseDriver {
+    public static WebDriver driver;
+    public static WebDriverWait bekle;
+
+    @BeforeClass
+    public void Setup()
+    {
+        driver = new ChromeDriver();
+
+        driver.manage().window().maximize(); // Ekranı max yapıyor.
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30)); // 20 sn mühlet: sayfayı yükleme mühlet
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // 5 sn mühlet: elementi bulma mühleti
+
+        bekle=new WebDriverWait(driver, Duration.ofSeconds(20));
+    }
+
+    @AfterClass
+    public void TearDown()
+    {
+        MyFunc.Bekle(3);
+        driver.quit();
+    }
+}
