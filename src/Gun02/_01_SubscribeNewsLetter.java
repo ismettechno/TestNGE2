1b6dfae1112b29d96977a3753b02c64a3c26dcae
TechnoStudy <ismettechno@gmail.com>
2026-05-07 19:48:16 +0100

    _01_SubscribeNewsLetter Soru

---
+++ src/Gun02/_01_SubscribeNewsLetter.java
@@ -0,0 +1,14 @@
+package Gun02;
+
+public class _01_SubscribeNewsLetter {
+       /*
+    Senaryo
+  1- Siteyi açınız.
+  2- Newsletter  Subscribe işlemini abone olunuz(YES)  , işlemin başarılı olduğunu kontrol ediniz.
+
+
+        */
+
+
+
+}
