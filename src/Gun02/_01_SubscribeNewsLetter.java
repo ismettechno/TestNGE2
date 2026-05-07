d71d69440c85fd4df7ef335c81557dabea4e5666
TechnoStudy <ismettechno@gmail.com>
2026-05-07 19:48:51 +0100

    _01_SubscribeNewsLetter Soru tümü

--- src/Gun02/_01_SubscribeNewsLetter.java
+++ src/Gun02/_01_SubscribeNewsLetter.java
@@ -5,7 +5,8 @@ public class _01_SubscribeNewsLetter {
     Senaryo
   1- Siteyi açınız.
   2- Newsletter  Subscribe işlemini abone olunuz(YES)  , işlemin başarılı olduğunu kontrol ediniz.
-
+  3- Ayrı bir test ile Newsletter  Subscribe işlemini abonelikten çıkınız(NO)
+  4- Ayrı bir test ile Newsletter  Subscribe durumunu kontrol ediniz YES ise NO, NO ise YES yapınız.
 
         */
 
