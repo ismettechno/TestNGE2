3ce69c9aef483ab9a9fafd53c9550f222b1ab76f
TechnoStudy <ismettechno@gmail.com>
2026-05-11 18:10:56 +0100

    SubscribeFunctionYes

--- src/Gun02/_01_SubscribeNewsLetter.java
+++ src/Gun02/_01_SubscribeNewsLetter.java
@@ -1,15 +1,44 @@
 package Gun02;
 
-public class _01_SubscribeNewsLetter {
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _01_SubscribeNewsLetter extends BaseDriver {
        /*
     Senaryo
   1- Siteyi açınız.
   2- Newsletter  Subscribe işlemini abone olunuz(YES)  , işlemin başarılı olduğunu kontrol ediniz.
   3- Ayrı bir test ile Newsletter  Subscribe işlemini abonelikten çıkınız(NO)
   4- Ayrı bir test ile Newsletter  Subscribe durumunu kontrol ediniz YES ise NO, NO ise YES yapınız.
+       */
 
-        */
+    @Test
+    public void SubscribeFunctionYes() {
+        WebElement newsLetterLink=driver.findElement(By.linkText("Newsletter"));
+        newsLetterLink.click();
 
+        WebElement subscribeYes=driver.findElement(By.xpath("//input[@type='radio' and @value='1']"));
+        subscribeYes.click();
 
+        WebElement continueButton=driver.findElement(By.xpath("//*[@value='Continue']"));
+        continueButton.click();
+
+        WebElement msgLabel=BaseDriver.driver.
+                findElement(By.cssSelector("[class='alert alert-success alert-dismissible']"));
+
+        Assert.assertTrue(msgLabel.getText().toLowerCase().contains("success"));
+    }
+
+    public void SubscribeFunctionNo() {
+
+    }
+
+    public void SubscribeFunctionForBoth()
+    {
+
+    }
 
 }
--- src/Utility/BaseDriver.java
+++ src/Utility/BaseDriver.java
@@ -1,8 +1,11 @@
 package Utility;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
@@ -22,7 +25,29 @@ public class BaseDriver {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5)); // 5 sn mühlet: elementi bulma mühleti
 
         bekle=new WebDriverWait(driver, Duration.ofSeconds(20));
+
+        LoginTesti();
     }
+
+    public void LoginTesti()
+    {
+        System.out.println("Login Testi başladı");
+        driver.get("https://opencart.abstracta.us/index.php?route=account/login");
+
+        WebElement email=driver.findElement(By.xpath("//input[@id='input-email']"));
+        email.sendKeys("testng1@gmail.com");
+
+        WebElement password=driver.findElement(By.xpath("//input[@id='input-password']"));
+        password.sendKeys("123qweasd");
+
+        WebElement loginBtn=driver.findElement(By.xpath("//*[@value='Login']"));
+        loginBtn.click();
+
+        Assert.assertTrue(driver.getTitle().equals("My Account"), "Login olunamadı");
+
+        System.out.println("Login Testi Bitti");
+    }
+
 
     @AfterClass
     public void TearDown()
