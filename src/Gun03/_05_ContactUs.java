e96d15adc8b4f72e8fd8639fa152e4911b47d70b
TechnoStudy <ismettechno@gmail.com>
2026-05-12 19:12:18 +0100

    _05_ContactUs

---
+++ src/Gun03/_05_ContactUs.java
@@ -0,0 +1,32 @@
+package Gun03;
+
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class _05_ContactUs extends BaseDriver {
+    /*
+  1- ContactUs a tıklayınız
+  2- Mesaj kutusuna en az 10 karakterlik bir mesaj yazınız.
+  3- Submit ettikten sonra url deki success yazısını doğrulayınız.
+ */
+
+    @Test
+    public void contactUs(){
+        WebElement contactUsBtn=driver.findElement(By.linkText("Contact Us"));
+        contactUsBtn.click();
+
+        WebElement enquiryArea=driver.findElement(By.id("input-enquiry"));
+        enquiryArea.sendKeys("Merhaba testing dünyası");
+
+        WebElement submitBtn=driver.findElement(By.cssSelector("[type='submit']"));
+        submitBtn.click();
+
+        bekle.until(ExpectedConditions.urlContains("success"));
+        Assert.assertTrue(driver.getCurrentUrl().contains("success"));
+    }
+
+}
