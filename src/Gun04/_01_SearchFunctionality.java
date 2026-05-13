047cb7bf823e27436fe9688ede3485a106bdfa6c
TechnoStudy <ismettechno@gmail.com>
2026-05-13 18:29:51 +0100

    SearchFunction

---
+++ src/Gun04/_01_SearchFunctionality.java
@@ -0,0 +1,39 @@
+package Gun04;
+
+import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class _01_SearchFunctionality extends BaseDriver {
+    /*
+      Senaryo ;
+      1- Siteyi açınız.
+      2- mac(xml den gönderilecek) kelimeini göndererek aratınız.
+      3- Çıkan sonuçlarda mac kelimesinin geçtiğini doğrulayınız.
+      4- aynı işlemi samsung(xml den gönderilecek için de yapınız
+    */
+
+    @Test
+    @Parameters("searchText")
+    public void SearchFunction(String aranacakKelime)
+    {
+        WebElement searchInput=driver.findElement(By.name("search"));
+        searchInput.sendKeys(aranacakKelime+ Keys.ENTER);
+
+        List<WebElement> captions=driver.findElements(By.xpath("//*[@class='caption']/h4/a"));
+
+        for (WebElement e : captions)
+            Assert.assertTrue(e.getText().toLowerCase().contains(aranacakKelime));
+    }
+
+
+
+
+
+}
